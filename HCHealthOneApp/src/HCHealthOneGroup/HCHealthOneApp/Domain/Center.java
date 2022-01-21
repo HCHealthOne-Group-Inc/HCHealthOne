2ffa248c0089944eb98617c79197e67a78925365
Tabassumc9 <tchou005@fiu.edu>
2022-01-21 11:17:18 -0500

    Classes
    Added Center and Person

---
+++ HCHealthOneApp/src/HCHealthOneGroup/HCHealthOneApp/Domain/Center.java
@@ -0,0 +1,19 @@
+package HCHealthOneGroup.HCHealthOneApp.Domain;
+
+public class Center {
+	public String Name;
+	public String SquareFeet;
+	public String NumberofRooms;
+	public String CenterType;
+	
+	/**
+	 * 
+	 * @param x to show coordinate of one side 
+	 * @param y to show the opposite side measurement 
+	 * @param Feet to show the calculated measurement
+	 */
+	public void Capacity(int x, int y, String Feet) {
+		//find capacity of the center
+	}
+	
+}
---
+++ HCHealthOneApp/src/HCHealthOneGroup/HCHealthOneApp/Domain/Person.java
@@ -0,0 +1,29 @@
+package HCHealthOneGroup.HCHealthOneApp.Domain;
+
+public class Person {
+	public String Address; 
+	public int dateOfBirth; 
+	public String LastName; 
+	public String FirstName;
+	public int identificationNumber;
+	public String Language;
+	public String SexAssignedAtBirth;
+	
+	/**
+	 * 
+	 * @param age to display the age
+	 * @param x show the age grabbed from the data
+	 */
+	public void Age(String age, int x) {
+		//get patient's age 
+	}
+	
+	public void Address() {
+		//get patient's current address 
+	}
+	
+	public void FullName() {
+		//get patient's full name
+	}
+	
+}
