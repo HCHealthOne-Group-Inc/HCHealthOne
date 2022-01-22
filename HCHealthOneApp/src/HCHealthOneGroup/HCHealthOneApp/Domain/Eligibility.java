d1649da957d3713f6a79612e2df1fb43730022a1
Couso10 <97350890+Couso10@users.noreply.github.com>
2022-01-21 19:32:36 -0500

    update
    update

---
+++ HCHealthOneApp/src/HCHealthOneGroup/HCHealthOneApp/Domain/Eligibility.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package HCHealthOneGroup.HCHealthOneApp.Domain;
+
+/**
+ * @author JavierC
+ *
+ */
+public class Eligibility {
+
+	private String Patient;
+    private String InsurancePlan; 
+    
+    public void IsEligible() {
+    
+    }
+}
+
